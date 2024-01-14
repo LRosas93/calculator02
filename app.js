93c35e485ac11053bb1e4ac5d1e95b9d21836740
LRosas93 <luisr1993@yahoo.com>
2024-01-14 15:52:42 -0800

    design looks great, added reset styling

--- app.js
+++ app.js
@@ -1,22 +1,73 @@
-const buttons = document.querySelectorAll('button');
-const outputValue = document.querySelector('.output-display');
+const buttons = document.querySelectorAll('.button');
+const display = document.querySelector('.display');
+const operatorButtons = document.querySelectorAll('.operator');
+let opButtons = [];
+// factory function to create buttons
+function createButtons() {
+    function createButton(button) {
+        return {
+            value: button.value,
+            active: false
+        }
+    }
+
+    for (let i = 0; i < operatorButtons.length; i++) {
+        let btn = createButton(operatorButtons[i]);
+        opButtons.push(btn);
+    }
+}
+
+const operatorList = {
+    '*': 'multiply',
+    '+': 'addition',
+    '-': 'subtraction',
+    '/': 'division'
+}
+
+createButtons();
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+
         if (button.textContent === 'AC') {
-            clearDisplay();
-        } else if (button.textContent ==='=') {
-            try {
-                clearDisplay.value = eval(outputValue.value);
-            } catch (error) {
-                outputValue.value = 'Error';
-            }
-        } else {
-            outputValue.value += button.textContent;
+            clearScreen();
         }
-});
+        // evaluates the expression
+        if (button.textContent === '=') {
+            try {
+                evaluateExpression();
+            } catch(error) {
+                alert("Error: " + error);
+            }
+        } else if (button.value in operatorList) {
+            toggleActive(button);
+            }
+        else {
+            addToDisplay(button);
+        }
+        }
+    )
 });
 
-function clearDisplay() {
-    outputValue.display = '';
+function clearScreen() {
+    display.textContent = '';
+}
+
+function evaluateExpression() {
+    const expression = eval(display.textContent);
+    display.textContent = expression;
+}
+
+function addToDisplay(btn) {
+    display.textContent += btn.value;
+    display.value += btn.value;
+}
+
+// operator buttons
+function toggleActive(btn) {
+    if (!(btn.classList.contains('operatorActive'))) {
+        btn.classList.toggle('operatorActive');
+    } else {
+        btn.classList.remove('operatorActive');
+    }
 }
