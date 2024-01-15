0755bb1c8d48c90c92dddc132706af1680c582f0
LRosas93 <luisr1993@yahoo.com>
2024-01-14 20:47:47 -0800

    switched h2 element for input element for display

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
 const buttons = document.querySelectorAll('.button');
 const display = document.querySelector('.display');
 const operatorButtons = document.querySelectorAll('.operator');
-let opButtons = [];
-// factory function to create buttons
-function createButtons() {
-    function createButton(button) {
-        return {
-            value: button.value,
-            active: false
-        }
-    }
 
-    for (let i = 0; i < operatorButtons.length; i++) {
-        let btn = createButton(operatorButtons[i]);
-        opButtons.push(btn);
-    }
-}
-
-const operatorList = {
-    '*': 'multiply',
-    '+': 'addition',
-    '-': 'subtraction',
-    '/': 'division'
-}
-
-createButtons();
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -39,9 +16,7 @@ buttons.forEach(button => {
             } catch(error) {
                 alert("Error: " + error);
             }
-        } else if (button.value in operatorList) {
-            toggleActive(button);
-            }
+        }
         else {
             addToDisplay(button);
         }
@@ -50,24 +25,19 @@ buttons.forEach(button => {
 });
 
 function clearScreen() {
-    display.textContent = '';
+    display.value = '';
 }
 
 function evaluateExpression() {
-    const expression = eval(display.textContent);
-    display.textContent = expression;
+    const result = eval(display.value);
+    display.value = result;
 }
 
 function addToDisplay(btn) {
-    display.textContent += btn.value;
     display.value += btn.value;
 }
 
 // operator buttons
 function toggleActive(btn) {
-    if (!(btn.classList.contains('operatorActive'))) {
-        btn.classList.toggle('operatorActive');
-    } else {
-        btn.classList.remove('operatorActive');
-    }
+    btn.classList.toggle('operatorActive');
 }
